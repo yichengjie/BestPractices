a2aa39d1636d3be34a2c182a0c1a2fd0acae11f9
yichengjie <626659321@qq.com>
2019-01-13 22:08:16 +0800

    范性

---
+++ src/lostinfomation/LostInfomation.java
@@ -0,0 +1,26 @@
+package lostinfomation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Forb{}
+class Fnorkle{}
+class Quark<Q>{}
+class Particle<POSITION,MOMENTUM>{}
+
+public class LostInfomation {
+	
+	public static void main(String[] args) {
+		List<Forb> list = new ArrayList<Forb>() ;
+		Map<Forb,Fnorkle> map = new HashMap<>() ;
+		Quark<Fnorkle> quark = new Quark<Fnorkle>() ;
+		Particle<Long,Double> p = new Particle<Long,Double>() ;
+		System.out.println(Arrays.toString(list.getClass().getTypeParameters()));
+		System.out.println(Arrays.toString(map.getClass().getTypeParameters()));
+		System.out.println(quark.getClass().getTypeParameters());
+		System.out.println(p.getClass().getTypeParameters());
+	}
+}
