d58485b521f028e6a9528e209019e38821f3f3f8
yichengjie <626659321@qq.com>
2019-01-15 22:49:46 +0800

    add

---
+++ src/com/yicj/demo/generics1/CreatorGeneric.java
@@ -0,0 +1,31 @@
+package com.yicj.demo.generics1;
+
+abstract class GenericWithCreate<T>{
+	final T element ;
+	public GenericWithCreate() {
+		element = create() ;
+	}
+	abstract T create() ;
+}
+
+class XXX{}
+
+class Creator extends GenericWithCreate<XXX>{
+	@Override
+	XXX create() {
+		return new XXX();
+	}
+	
+	void f() {
+		System.out.println(element.getClass().getSimpleName());
+	}
+}
+
+public class CreatorGeneric {
+	
+	public static void main(String[] args) {
+		Creator c = new Creator() ;
+		c.f();
+	}
+
+}
