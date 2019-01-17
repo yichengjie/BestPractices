033be8dc74e2bc01c6d18ad11a8d6a4cf8b19a9e
yichengjie <626659321@qq.com>
2019-01-17 10:08:30 +0800

    jaxb

---
+++ src/com/yicj/demo/jaxb/JaxbUtil.java
@@ -0,0 +1,42 @@
+package com.yicj.demo.jaxb;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+public class JaxbUtil {
+	
+	public static void convertToXml(Object obj ,File file){
+		try {
+			Class<? extends Object> clazz = obj.getClass();
+			JAXBContext jaxbContext = JAXBContext.newInstance(clazz) ;
+			Marshaller marshaller = jaxbContext.createMarshaller();
+			//格式化输出，即按标签自动换行，否则就是一行输出
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			//设置编码（默认编码就是utf-8）
+			marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+			//是否省略xml头信息，默认不省略（false）
+			marshaller.setProperty(Marshaller.JAXB_FRAGMENT, false);
+			marshaller.marshal(obj, file);
+			//控制台输出
+			marshaller.marshal(obj,System.out) ;
+		} catch (JAXBException e) {
+			throw new RuntimeException(e) ;
+		}
+	}
+	
+	public static <T> T convertToJavaBean(Class<T> clz, File file) {
+	    try {
+	        JAXBContext jaxbContext = JAXBContext.newInstance(clz);
+	        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+	        T t = (T) unmarshaller.unmarshal(file);
+	        return t;
+	    } catch (JAXBException e) {
+	        throw new RuntimeException(e) ;
+	    }
+	}
+
+}
---
+++ src/com/yicj/demo/jaxb/User.java
@@ -0,0 +1,76 @@
+package com.yicj.demo.jaxb;
+
+import javax.xml.bind.annotation.*;
+import java.io.Serializable;
+
+//JavaBean代码
+
+@XmlType(propOrder = {})
+@XmlRootElement(name = "user")
+@XmlAccessorType(XmlAccessType.PROPERTY)
+@XmlAccessorOrder(XmlAccessOrder.ALPHABETICAL)
+public class User implements Serializable {
+
+    private String userName;
+    private int age;
+    private String role;
+    private String bibi;
+
+    public User() {
+    }
+
+    public User(String userName, int age, String role, String bibi) {
+        this.userName = userName;
+        this.age = age;
+        this.role = role;
+        this.bibi = bibi;
+    }
+    
+    @XmlElement(required = true)
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @XmlElement
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @XmlElement
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @XmlTransient
+    public String getBibi() {
+        return bibi;
+    }
+
+    public void setBibi(String bibi) {
+        this.bibi = bibi;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", age=" + age +
+                ", role='" + role + '\'' +
+                ", bibi='" + bibi + '\'' +
+                '}';
+    }
+}
+
+//测试
