ac861a25d0b4117fc2dc3820152255c5abbb42e8
yichengjie <626659321@qq.com>
2019-01-22 22:38:02 +0800

    范型-装饰实现

---
+++ src/com/yicj/demo/generics6/Decoration.java
@@ -0,0 +1,61 @@
+package com.yicj.demo.generics6;
+
+import java.util.Date;
+
+class Basic{
+	private String value ;
+	public void set(String val) {
+		this.value = val ;
+	}
+	public String get() {
+		return this.value ;
+	}
+}
+
+class Decorator extends Basic{
+	protected Basic basic ;
+	public Decorator(Basic basic) {
+		this.basic = basic ;
+	}
+	public void set(String val) {
+		this.basic.set(val);
+	}
+	public String get() {
+		return this.basic.get() ;
+	}
+}
+
+class TimeStamped extends Decorator{
+	private final long timeStamp ;
+	public TimeStamped(Basic basic) {
+		super(basic) ;
+		timeStamp = new Date().getTime() ;
+	}
+	public long getStamp() {
+		return timeStamp ;
+	}
+}
+
+class SerialNumbered extends Decorator{
+	private static long counter = 1;
+	private final long serialNumber = counter ++ ;
+	public SerialNumbered(Basic basic) {
+		super(basic);
+	}
+	public long getSerialNumber() {
+		return serialNumber ;
+	}
+}
+
+
+public class Decoration {
+	
+	public static void main(String[] args) {
+		TimeStamped t = new TimeStamped(new Basic()) ;
+		TimeStamped t2 = new TimeStamped(
+				new SerialNumbered(new Basic())) ;
+		SerialNumbered s = new SerialNumbered(new Basic()) ;
+		SerialNumbered s2 = new SerialNumbered(
+				new TimeStamped(new Basic())) ;
+	}
+}
