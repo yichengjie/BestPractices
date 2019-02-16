622a30363a329f2e86ecbc418ae5ee5cb917eb59
yichengjie <626659321@qq.com>
2019-02-16 17:17:17 +0800

    m

---
+++ src/com/yicj/demo/constructors/PolyConstructors.java
@@ -0,0 +1,34 @@
+package com.yicj.demo.constructors;
+import static com.yicj.demo.CommonUtil.print ;
+
+
+class Glyph {
+	void draw() {
+		print("Glyph.draw()") ;
+	}
+	public Glyph() {
+		print("Glyph() before draw()");
+		draw();
+		print("Glyph() after draw()");
+	}
+}
+
+class RoundGlyph extends Glyph{
+	private int radius = 1 ;
+	public RoundGlyph(int r) {
+		radius = r ;
+		print("RoundGlyph.RoundGlyph(). radius = " + radius);
+	}
+	
+	void draw() {
+		print("RoundGlyph.draw(). radius = " + radius);
+	}
+}
+
+
+public class PolyConstructors {
+
+	public static void main(String[] args) {
+		new RoundGlyph(5) ;
+	}
+}
