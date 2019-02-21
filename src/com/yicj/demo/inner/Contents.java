d6c499f102cba7f898b92145e74cd252dad4ce78
yichengjie <626659321@qq.com>
2019-02-21 21:18:11 +0800

    inner class demo1

---
+++ src/com/yicj/demo/inner/Contents.java
@@ -0,0 +1,6 @@
+package com.yicj.demo.inner;
+
+public interface Contents {
+
+	int value() ;
+}
---
+++ src/com/yicj/demo/inner/Destination.java
@@ -0,0 +1,6 @@
+package com.yicj.demo.inner;
+
+public interface Destination {
+	
+	String readLabel() ;
+}
