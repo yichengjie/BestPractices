7db042442fe470c74cf07b1a678419f98baa4858
yichengjie <626659321@qq.com>
2019-02-24 22:50:57 +0800

    demo3

---
+++ src/com/yicj/demo/inner2/Event.java
@@ -0,0 +1,20 @@
+package com.yicj.demo.inner2;
+
+public abstract class Event {
+	
+	private long eventTime ;
+	protected final long delayTime ;
+	public Event (long delayTime) {
+		this.delayTime = delayTime ;
+		start() ;
+	}
+	//Allows restarting
+	public void start() {
+		eventTime = System.nanoTime() + delayTime;
+	}
+	
+	public boolean ready() {
+		return System.nanoTime() >= eventTime ;
+	}
+	public abstract void action() ;
+}
