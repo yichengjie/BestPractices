56897a0c376cb3e10161ae0465e9e4aeecd16e23
yichengjie <626659321@qq.com>
2019-03-04 22:22:30 +0800

    file2

---
+++ src/com/yicj/demo/file/DirectoryDemo.java
@@ -0,0 +1,31 @@
+package com.yicj.demo.file;
+
+import java.io.File;
+import static com.yicj.demo.util.CommonUtil.println;
+
+import com.yicj.demo.util.Directory;
+import com.yicj.demo.util.PPrint;
+
+public class DirectoryDemo {
+	
+	public static void main(String[] args) {
+		
+		//All directories
+		PPrint.pprint(Directory.walk(".").dirs);
+		//All files beginning with 'T'
+		for(File file : Directory.local(".", "T.*")) {
+			println(file) ;
+		}
+		println("----------------------------------") ;
+		// All Java files beginning with 'T'
+		for(File file : Directory.walk(".","T.*\\.java")) {
+			println(file) ;
+		}
+		println("===================================") ;
+		//Class files containing 'Z' or 'z'
+		for(File file : Directory.walk(".",".*[Zz].*\\.class")) {
+			println(file) ;
+		}
+	}
+
+}
