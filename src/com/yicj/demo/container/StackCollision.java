5b12d28fcb4a1f29bdd0baf7cc77c065a4b27bcb
yichengjie <626659321@qq.com>
2019-03-11 22:30:37 +0800

    容器深入

---
+++ src/com/yicj/demo/container/StackCollision.java
@@ -0,0 +1,13 @@
+package com.yicj.demo.container;
+
+import java.util.Stack;
+
+public class StackCollision {
+	
+	public static void main(String[] args) {
+		
+		Stack<String> stack ;
+		
+		
+	}
+}
---
+++ src/com/yicj/demo/container2/CollectionDataTest.java
@@ -0,0 +1,31 @@
+package com.yicj.demo.container2;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import com.yicj.demo.generator1.Generator;
+
+class Government implements Generator<String>{
+	String [] foundation = ("strange women lying in ponds "+
+		"distributing swrds is no basis for a system of "+
+		"government").split(" ") ;
+	private int index ;
+	@Override
+	public String next() {
+		return foundation[index++];
+	}
+}
+
+
+public class CollectionDataTest {
+	
+	public static void main(String[] args) {
+		
+		Set<String> set = new LinkedHashSet<String>(
+			new CollectionData<String>(new Government(), 15)) ;
+		// Using the convenience method
+		set.addAll(CollectionData.list(new Government(), 15)) ;
+		System.out.println(set);
+	}
+
+}
