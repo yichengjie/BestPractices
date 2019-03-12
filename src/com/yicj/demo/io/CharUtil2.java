2d43a643fd14f047b7fb8b61df1d2ad501947290
yichengjie <626659321@qq.com>
2019-03-12 22:10:23 +0800

    io处理

---
+++ src/com/yicj/demo/io/CharUtil2.java
@@ -0,0 +1,36 @@
+package com.yicj.demo.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+public class CharUtil2 {
+	
+	public static void dump(Reader src, Writer dest) 
+			throws IOException{
+		
+		try {
+			char [] data = new char[1024] ;
+			int len; 
+			while( (len = src.read(data)) != -1 )
+				dest.write(data, 0, len);
+		} finally {
+			src.close();
+			dest.close();
+		}
+	}
+	
+	public static void dump(InputStream src, OutputStream dest,
+			String charset) throws IOException {
+		dump(new InputStreamReader(src, charset), 
+			new OutputStreamWriter(dest,charset));
+	}
+	
+	public static void dump(InputStream src, OutputStream dest) throws IOException {
+		dump(src, dest,System.getProperty("file.encoding"));
+	}
+}
---
+++ src/com/yicj/demo/io/CharUtil3.java
@@ -0,0 +1,47 @@
+package com.yicj.demo.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+public class CharUtil3 {
+	//FileReader/RileWriter则可以对文档做读取与写入，
+	//读取或写入时默认会使用操作系统默认编码来做字符转化，
+	//在启动jvm时，可以指定-Dfile.encoding来指定FileReader/FileWriter所使用的编码
+	//FileReader/FileWriter没有可以指定编码的方法，如果在程序执行过程中想要指定比编码，
+	//则必须使用InputStreamReader/OutputStreamWriter/者两个类实际上时作为装饰器。
+	
+	
+	public static void dump(Reader src, Writer dest) 
+			throws IOException{
+		BufferedReader input = null ;
+		BufferedWriter output = null ;
+		try {
+			input = new BufferedReader(src) ;
+			output = new BufferedWriter(dest) ; 
+			char [] data = new char[1024] ;
+			int len; 
+			while( (len = input.read(data)) != -1 )
+				output.write(data, 0, len);
+		} finally {
+			input.close();
+			output.close();
+		}
+	}
+	
+	public static void dump(InputStream src, OutputStream dest,
+			String charset) throws IOException {
+		dump(new InputStreamReader(src, charset), 
+			new OutputStreamWriter(dest,charset));
+	}
+	
+	public static void dump(InputStream src, OutputStream dest) throws IOException {
+		dump(src, dest,System.getProperty("file.encoding"));
+	}
+}
