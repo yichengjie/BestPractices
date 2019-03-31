6020ce23751fe6197021d7ca55f3e844ceaaaef3
yichengjie <626659321@qq.com>
2019-03-31 22:38:34 +0800

    lambda表达式学习

--- src/com/yicj/demo/lambda/MethodReference.java
+++ src/com/yicj/demo/lambda/MethodReference.java
@@ -11,12 +11,23 @@ interface  Func<P, R> {
     //void add() ;
 }
 
+class A{
+    public int byLength(String name1, String name2){
 
-public class Demo01 {
+        return name1.length() - name2.length() ;
+    }
+}
+
+
+public class MethodReference {
 
     public static void main(String [] args){
 
+        A a = new A() ;
+
         Func<String,Integer> func = (name1, name2) -> name1.length() - name2.length() ;
+
+        Comparator<String> tt = a::byLength ;
 
         Comparator<String> comparator = (name1, name2) -> name1.length() - name2.length() ;
 
