992f0b83c8bad704d5c1ed8d1b9a4b26bb5174a0
yichengjie <626659321@qq.com>
2019-04-08 22:43:57 +0800

    转为maven项目

---
+++ src/test/java/com/yicj/demo/jaxb/App1.java
@@ -0,0 +1,24 @@
+package com.yicj.demo.jaxb;
+
+
+import org.junit.Test;
+
+import java.io.File;
+
+public class App1 {
+    @Test
+    public void saveXmlTest() {
+        User user = new User("陈本布衣", 2018, "超级管理员","瞎哔哔");
+        user.setUserName("");
+        File file = new File("E://user.xml");
+        JaxbUtil.convertToXml(user, file);
+    }
+
+    @Test
+    public void getUserTest() {
+    	File file = new File("E://user.xml");
+    	User user = JaxbUtil.convertToJavaBean(User.class, file);
+    	System.out.println(user);
+    	
+    }
+}
