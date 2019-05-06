c8e7e09316358614a6b941ec38d1e6ae47780582
yichengjie <626659321@qq.com>
2019-05-06 18:34:01 +0800

    redis

--- src/test/java/com/yicj/demo/redis/SentineTest2.java
+++ src/test/java/com/yicj/demo/redis/SentineTest2.java
@@ -13,22 +13,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Created by zhoum on 2018-06-22.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:spring-redis.xml"})
-public class RedisTest {
+@ContextConfiguration(locations = {"classpath*:spring-sentine.xml"})
+public class SentineTest2 {
     @Autowired
     private RedisTemplate redisTemplate;
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
     @Test
     public void test1(){
         ValueOperations valueOperations = redisTemplate.opsForValue();
-        valueOperations.set("first","hellow word");
+        //valueOperations.set("first","hellow word");
         System.out.println(valueOperations.get("first"));
-    }
-    @Test
-    public void test2(){
-        ValueOperations<String, String> stringStringValueOperations = stringRedisTemplate.opsForValue();
-        stringStringValueOperations.set("secound","hello everyone");
-        System.out.println(stringStringValueOperations.get("secound"));
     }
 }
