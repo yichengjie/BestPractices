ce043a460985ca271f199e37567f666459c063e6
yichengjie <626659321@qq.com>
2019-10-12 21:21:06 +0800

    练习多线程

--- thread-study/src/main/java/com/yicj/thread/lock3/HorseRace.java
+++ thread-study/src/main/java/com/yicj/thread/lock3/HorseRace.java
@@ -1,10 +1,55 @@
 package com.yicj.thread.lock3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.*;
 
 public class HorseRace {
+
+    static final int FINISH_LINE = 75 ;
+    private List<Horse> horses = new ArrayList<>() ;
+    private ExecutorService pool = Executors.newCachedThreadPool() ;
+    private CyclicBarrier barrier ;
+    public HorseRace(int nHorses ,final int pause){
+        barrier = new CyclicBarrier(nHorses, new Runnable() {
+            @Override
+            public void run() {
+                StringBuilder s = new StringBuilder() ;
+                for(int i = 0 ; i < FINISH_LINE ; i++){
+                    s.append("=") ;
+                }
+                System.out.println(s);
+                for (Horse horse: horses){
+                    System.out.println(horse.tracks());
+                }
+                for(Horse horse: horses){
+                    if(horse.getStrides() >= FINISH_LINE){
+                        System.out.println(horse + " won!");
+                        pool.shutdownNow() ;
+                        return;
+                    }
+                }
+                try {
+                    TimeUnit.MILLISECONDS.sleep(pause);
+                } catch (InterruptedException e) {
+                    System.out.println("barrier-action sleep interrupted");
+                }
+            }
+        }) ;
+        for (int i = 0 ; i < nHorses; i++){
+            Horse horse = new Horse(barrier) ;
+            horses.add(horse) ;
+            pool.execute(horse);
+        }
+    }
+
+    public static void main(String[] args) {
+        int nHorses = 7 ;
+        int pause = 200 ;
+        new HorseRace(nHorses,pause) ;
+    }
+
 
 
     private static class Horse implements Runnable{
@@ -32,7 +77,21 @@ public class HorseRace {
                 // A legitimate way to exit
             }catch (BrokenBarrierException e){
                 // This one we want to know about
+                throw new RuntimeException(e) ;
             }
+        }
+
+        @Override
+        public String toString() {
+            return "Horse " + id +"" ;
+        }
+        public String tracks(){
+            StringBuilder s = new StringBuilder() ;
+            for(int i = 0 ; i < getStrides(); i++){
+                s.append("*") ;
+            }
+            s.append(" " + id) ;
+            return s.toString() ;
         }
     }
 
