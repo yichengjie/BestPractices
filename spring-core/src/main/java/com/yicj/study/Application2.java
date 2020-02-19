f99da0cebfb67a4d79aa6ccdbcd068ada54f695e
易成杰 <626659321@qq.com>
2020-02-19 22:11:46 +0800

    保存

--- spring-core/src/main/java/com/yicj/study/Application2.java
+++ spring-core/src/main/java/com/yicj/study/Application2.java
@@ -4,10 +4,11 @@ import com.yicj.study.scanner.ClassScanner2;
 
 import java.util.Set;
 
+
 public class Application2 {
 
     public static void main(String[] args) throws Exception {
-        Set<Class<?>> classes = ClassScanner2.getClasses("com.yicj");
+        Set<Class<?>> classes = ClassScanner2.getClasses("com.yicj.study");
         classes.forEach(name-> System.out.println(name));
     }
 }
