0221daa3a56c944a9d2e688ef57de63eb5b1c979
易成杰 <626659321@qq.com>
2020-02-20 17:08:26 +0800

    保存

--- spring-core/src/main/java/com/yicj/study/MiniApplication.java
+++ spring-core/src/main/java/com/yicj/study/MiniApplication.java
@@ -3,7 +3,7 @@ package com.yicj.study;
 import com.yicj.study.bean.BeanFactory;
 import com.yicj.study.handler.HandlerManager;
 import com.yicj.study.handler.MappingHandler;
-import com.yicj.study.scanner.ClassScanner2;
+import com.yicj.study.scanner.ClassScanner;
 import com.yicj.study.server.TomcatServer;
 import java.util.List;
 
@@ -12,7 +12,7 @@ public class MiniApplication {
     public static void run(Class<?> cls, String[] args){
         TomcatServer tomcatServer = new TomcatServer(args);
         tomcatServer.startServer();
-        List<Class<?>> classes = ClassScanner2.scanClasses(cls.getPackage().getName());
+        List<Class<?>> classes = ClassScanner.scanClasses(cls.getPackage().getName());
         BeanFactory.initBean(classes);
         HandlerManager.resolveMappingHandler(classes); ;
         //classes.forEach(clazz -> System.out.println(clazz));
