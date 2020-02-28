827738920d5eb6231d927322ad6f6f853213d8dc
易成杰 <626659321@qq.com>
2020-02-28 17:01:36 +0800

    校验单元测试

--- hibernate-validator-study/src/main/java/com/yicj/study/hello/ValidationUtil.java
+++ hibernate-validator-study/src/main/java/com/yicj/study/hello/ValidationUtil.java
@@ -1,17 +1,12 @@
 package com.yicj.study.hello;
 
-import lombok.Data;
 import org.hibernate.validator.HibernateValidator;
-
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 public class ValidationUtil {
-
     /**
      * 开启快速结束模式 failFast (true)
      */
@@ -25,8 +20,8 @@ public class ValidationUtil {
      * @param groups 校验组
      * @return ValidResult
      */
-    public static <T> ValidResult validateBean(T t,Class<?>...groups) {
-        ValidResult result = new ValidationUtil().new ValidResult();
+    public static <T> ValidResult validateBean(T t, Class<?>...groups) {
+        ValidResult result = new ValidResult();
         Set<ConstraintViolation<T>> violationSet = validator.validate(t,groups);
         boolean hasError = violationSet != null && violationSet.size() > 0;
         result.setHasErrors(hasError);
@@ -45,7 +40,7 @@ public class ValidationUtil {
      * @return ValidResult
      */
     public static <T> ValidResult validateProperty(T obj, String propertyName) {
-        ValidResult result = new ValidationUtil().new ValidResult();
+        ValidResult result = new ValidResult();
         Set<ConstraintViolation<T>> violationSet = validator.validateProperty(obj, propertyName);
         boolean hasError = violationSet != null && violationSet.size() > 0;
         result.setHasErrors(hasError);
@@ -56,68 +51,6 @@ public class ValidationUtil {
         }
         return result;
     }
-    /**
-     * 校验结果类
-     */
-    @Data
-    public class ValidResult {
-        /**
-         * 是否有错误
-         */
-        private boolean hasErrors;
-        /**
-         * 错误信息
-         */
-        private List<ErrorMessage> errors;
 
-        public ValidResult() {
-            this.errors = new ArrayList<>();
-        }
-        public boolean hasErrors() {
-            return hasErrors;
-        }
 
-        public void setHasErrors(boolean hasErrors) {
-            this.hasErrors = hasErrors;
-        }
-
-        /**
-         * 获取所有验证信息
-         * @return 集合形式
-         */
-        public List<ErrorMessage> getAllErrors() {
-            return errors;
-        }
-        /**
-         * 获取所有验证信息
-         * @return 字符串形式
-         */
-        public String getErrors(){
-            StringBuilder sb = new StringBuilder();
-            for (ErrorMessage error : errors) {
-                sb.append(error.getPropertyPath()).append(":").append(error.getMessage()).append(" ");
-            }
-            return sb.toString();
-        }
-
-        public void addError(String propertyName, String message) {
-            this.errors.add(new ErrorMessage(propertyName, message));
-        }
-    }
-
-    @Data
-    public class ErrorMessage {
-
-        private String propertyPath;
-
-        private String message;
-
-        public ErrorMessage() {
-        }
-
-        public ErrorMessage(String propertyPath, String message) {
-            this.propertyPath = propertyPath;
-            this.message = message;
-        }
-    }
 }
--- hibernate-validator-study/src/test/java/com/yicj/study/hello/ValidationUtilTest.java
+++ hibernate-validator-study/src/test/java/com/yicj/study/hello/ValidationUtilTest.java
@@ -5,12 +5,28 @@ import org.junit.Test;
 public class ValidationUtilTest {
 
     @Test
-    public void test5() {
+    public void testValidateBean() {
         Account account = new Account();
         account.setAlias("kalakala");
         account.setUserName("wokalakala");
         account.setPassWord("密码");
-        ValidationUtil.ValidResult validResult = ValidationUtil.validateBean(account);
+        account.setLevel(800);
+
+        ValidResult validResult = ValidationUtil.validateBean(account);
+        if(validResult.hasErrors()){
+            String errors = validResult.getErrors();
+            System.out.println(errors);
+        }
+    }
+
+    @Test
+    public void testValidateProperty(){
+        Account account = new Account();
+        account.setAlias("kalakala");
+        account.setUserName("wokalakala");
+        account.setPassWord("密码");
+        account.setLevel(800);
+        ValidResult validResult = ValidationUtil.validateProperty(account,"level") ;
         if(validResult.hasErrors()){
             String errors = validResult.getErrors();
             System.out.println(errors);
